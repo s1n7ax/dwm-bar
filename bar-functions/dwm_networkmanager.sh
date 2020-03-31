263bd7e7e000192600dea0d7c7ec6a78981f9f1f
s1n7ax <srineshnisala@gmail.com>
2020-03-31 16:06:48 +0530

    [UPD] update scripts to personal preference

--- bar-functions/dwm_networkmanager.sh
+++ bar-functions/dwm_networkmanager.sh
@@ -7,19 +7,13 @@
 # Dependencies: NetworkManager, curl
 
 dwm_networkmanager () {
-    CONNAME=$(nmcli -a | grep 'Wired connection' | awk 'NR==1{print $1}')
-    if [ "$CONNAME" = "" ]; then
-        CONNAME=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -c 5-)
-    fi
-
-    PRIVATE=$(nmcli -a | grep 'inet4 192' | awk '{print $2}')
-    PUBLIC=$(curl -s https://ipinfo.io/ip)
+    CONNAME=$(iwgetid -r)
 
     printf "%s" "$SEP1"
     if [ "$IDENTIFIER" = "unicode" ]; then
-        printf "🌐 %s %s | %s" "$CONNAME" "$PRIVATE" "$PUBLIC"
+        printf "🌐 %s" "$CONNAME"
     else
-        printf "NET %s %s | %s" "$CONNAME" "$PRIVATE" "$PUBLIC"
+        printf "NET %s" "$CONNAME"
     fi
     printf "%s\n" "$SEP2"
 }
--- bar-functions/dwm_resources.sh
+++ bar-functions/dwm_resources.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
 # A dwm_bar function to display information regarding system memory, CPU temperature, and storage
-# Joe Standring <git@joestandring.com>
+# Srinesh Nisala<srineshnisala@gmail.com>
 # GNU GPLv3
 
 dwm_resources () {
+
     # Used and total memory
     MEMUSED=$(free -h | awk '(NR == 2) {print $3}')
     MEMTOT=$(free -h |awk '(NR == 2) {print $2}')
-    # CPU temperature
-    CPU=$(sysctl -n hw.sensors.cpu0.temp0 | cut -d. -f1)
-    # Used and total storage in /home (rounded to 1024B)
-    STOUSED=$(df -h | grep '/home$' | awk '{print $3}')
-    STOTOT=$(df -h | grep '/home$' | awk '{print $2}')
-    STOPER=$(df -h | grep '/home$' | awk '{print $5}')
+
+    # CPU temperature mean
+    CPU=$(sensors | grep '^Core.*' | awk '{ gensub(/\+(\\d)°C/, "\\1", "g", $3) } {s+=$3} END {print s/NR"%"}')
+
+    # Used and total storage in / (rounded to 1024B)
+    STOUSED=$(df -h | grep '/$' | awk '{print $3}')
+    STOTOT=$(df -h | grep '/$' | awk '{print $2}')
+    STOPER=$(df -h | grep '/$' | awk '{print $5}')
 
     printf "%s" "$SEP1"
     if [ "$IDENTIFIER" = "unicode" ]; then
